439d6b0dbe3d21cf8ca9470eba6bf12aa531aa13
ZoeHorwitz <99327981+ZoeHorwitz@users.noreply.github.com>
2022-03-20 21:26:20 +0100

    fps en assets importen

--- LaserGame3DUnity/Assets/Scripts/PlayerMovement.cs
+++ LaserGame3DUnity/Assets/Scripts/PlayerMovement.cs
@@ -21,7 +21,7 @@ public class PlayerMovement : MonoBehaviour
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if(isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
@@ -32,7 +32,7 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
@@ -40,4 +40,5 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(velocity * Time.deltaTime);
     }
+
 }
